452fced80788d7388f6a3b0d7761313662c2b1cf
nazaruys <nazary.box@gmail.com>
2024-10-13 16:58:38 +0200

    Initial Commit

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import colors from '../config/colors';
 
 function Screen({ children, style }) {
     return (
